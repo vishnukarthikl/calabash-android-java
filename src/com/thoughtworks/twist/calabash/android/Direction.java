0989fd5ea9fb0a05a1342f5d58a32234de1edab0
vishnu karthik <karthikl@thoughtworks.com>
2014-01-27 16:51:52 +0530

    adding up and down to direction

--- src/com/thoughtworks/twist/calabash/android/Direction.java
+++ src/com/thoughtworks/twist/calabash/android/Direction.java
@@ -2,15 +2,14 @@ package com.thoughtworks.twist.calabash.android;
 
 public enum Direction {
 
-	LEFT("left"), RIGHT("right");
+    LEFT("left"), RIGHT("right"), UP("up"), DOWN("down");
+    private final String direction;
 
-	private final String direction;
-
-	private Direction(String direction) {
+    private Direction(String direction) {
         this.direction = direction;
-	}
+    }
 
-	public String getDirection() {
-		return direction;
-	}
+    public String getDirection() {
+        return direction;
+    }
 }
