c0b140db2a6fb5fec5590fcc3a930ebb7dc13f72
prateekbaheti <prateektheone@gmail.com>
2014-02-13 12:42:17 +0530

    Monkey patching kernel.system call in ruby to avoid jvm crash while running the command to start the test server

--- src/com/thoughtworks/calabash/android/CalabashWrapper.java
+++ src/com/thoughtworks/calabash/android/CalabashWrapper.java
@@ -164,6 +164,19 @@ public class CalabashWrapper {
             script.append("def embed(path,image_type,file_name)\nend\n");
         }
 
+        //HACK - Jruby system call fails crashing the JVM on attempting to start test server command which redirects error stream to input stream.
+        //Overriding kernel system call to execute command via backick for the particular edge case. Rest of the calls will be executed via the regular
+        //kernel system call. Bug has been reported on jruby - https://github.com/jruby/jruby/issues/1500
+        //TODO: Remove this once the bug is fixed on jruby and the new jruby jar is added.
+        script.append(" def system(cmd)\n" +
+                "if (cmd.include? \"sh.calaba.instrumentationbackend.InstrumentationBackend\")\n" +
+                "     `#{cmd}`\n" +
+                "     return $?.success?\n" +
+                "  else\n" +
+                "   Kernel.system cmd\n" +
+                "  end\n" +
+                " end");
+
         container.runScriptlet(script.toString());
     }
 
