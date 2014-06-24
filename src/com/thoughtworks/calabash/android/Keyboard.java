b77d399b8bca5d72c8939e9733769d89f4549207
Mike Chabot <mike.chabot@live.com>
2014-06-24 09:48:53 -0400

    Fixed formatting issue

--- src/com/thoughtworks/calabash/android/Keyboard.java
+++ src/com/thoughtworks/calabash/android/Keyboard.java
@@ -7,79 +7,83 @@ import com.thoughtworks.calabash.android.Environment;
 import com.thoughtworks.calabash.android.EnvironmentInitializer;
 
 /**
- * Sometimes "set_text" doesn't work properly in Calabash, so we need to simulate
- * the behavior of the physical keyboard. We can do this through ADB:
+ * Sometimes "set_text" doesn't work properly in Calabash, so we need to
+ * simulate the behavior of the physical keyboard. We can do this through ADB:
  * 
- * http://krazyrobot.com/2014/02/calabash-android-enter-text-from-keyboard-using-adb/
+ * http://krazyrobot.com/2014/02/calabash-android-enter-text-from-keyboard-using
+ * -adb/
  * 
  * @author Mike Chabot
- *
+ * 
  */
 public class Keyboard {
-	
-    private static AndroidBridge bridge;
-	
-    public Keyboard(AndroidConfiguration androidConfiguration) {
-    	try {
-    		Environment environment = EnvironmentInitializer.initialize(androidConfiguration);
-    		bridge = new AndroidBridge(environment);
-    	} catch (CalabashException e) {
-    		System.out.println("Unable to initialize environment");
-    		System.out.println(e.getMessage());
-    	}
-    }
-    
-    /**
-     * Send text input to the device
-     * @param text
-     */
-    public void writeTextEvent(String text) {
-    	try {
+
+	private static AndroidBridge bridge;
+
+	public Keyboard(AndroidConfiguration androidConfiguration) {
+		try {
+			Environment environment = EnvironmentInitializer.initialize(androidConfiguration);
+			bridge = new AndroidBridge(environment);
+		} catch (CalabashException e) {
+			System.out.println("Unable to initialize environment");
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Send text input to the device
+	 * 
+	 * @param text
+	 */
+	public void writeTextEvent(String text) {
+		try {
 			bridge.sendTextEvent(text);
 			pause();
 		} catch (CalabashException e) {
 			System.out.println("Unable to send text event");
 		}
-    }
-    
-    /**
-     * Send text input to the device
-     * @param text
-     */
-    public void writeTextEvent(int integer) {
-    	try {
+	}
+
+	/**
+	 * Send text input to the device
+	 * 
+	 * @param text
+	 */
+	public void writeTextEvent(int integer) {
+		try {
 			bridge.sendTextEvent(String.valueOf(integer));
 			pause();
 		} catch (CalabashException e) {
 			System.out.println("Unable to send text event");
 		}
-    }
-    
-    /**
-     * Send key strokes to the device
-     * @param event
-     */
-    public void writeKeyEvent(String event) {
-    	try {
+	}
+
+	/**
+	 * Send key strokes to the device
+	 * 
+	 * @param event
+	 */
+	public void writeKeyEvent(String event) {
+		try {
 			bridge.sendKeyEvent(event);
 			pause();
 		} catch (CalabashException e) {
 			System.out.println("Unable to send text event");
 		}
-    }
-    
-    /**
-     * Pause the thread
-     */
-    private void pause() {
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-        	System.out.println("Error pausing keyboard");
-        }
-    }
-    
-    public void pressBackspace() {
-    	writeKeyEvent("KEYCODE_DEL");
-    }
+	}
+
+	/**
+	 * Pause the thread
+	 */
+	private void pause() {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			System.out.println("Error pausing keyboard");
+		}
+	}
+
+	public void pressBackspace() {
+		writeKeyEvent("KEYCODE_DEL");
+	}
 }
